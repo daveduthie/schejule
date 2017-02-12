67f12ddbf636d3a4868178e98c0992c98d06c63c
David Duthie <dave.a.duthie@gmail.com>
2017-02-13 02:03:36 +0800

    initial commit

---
+++ src/schejule/core.clj
@@ -0,0 +1,115 @@
+(ns schejule.core
+  (:refer-clojure :exclude [==])
+  (:require [clojure.core.logic :refer [== all everyg lvar run* succeed] :as l]
+            [clojure.core.logic.fd :as fd]))
+
+;; ## define helper macro to timeout a blocking function call
+;; [Stack Overflow](http://stackoverflow.com/questions/6694530/executing-a-function-with-a-timeout/6697469#6697469)
+(defmacro with-timeout [millis & body]
+  `(let [future# (future ~@body)]
+     (try
+       (.get future# ~millis java.util.concurrent.TimeUnit/MILLISECONDS)
+       (catch java.util.concurrent.TimeoutException x#
+         (do
+           (future-cancel future#)
+           nil)))))
+
+;; ## Ratiocinate
+(defn sort-tasks
+  "Sorts tasks by `:job`, then by `:id` to prepare for `solvo`"
+  [tasks]
+  (->> tasks (sort-by :job) (sort-by :id)))
+
+(defn init-jobs
+  "Binds tasks in a job to occur in the correct order.
+  `lengths` must be a seq of integers defining no. of tasks in nth job."
+  [tasks lengths]
+  (if (seq lengths)
+    (let [l (first lengths)]
+      (all
+       (everyg (fn [[x y]] (fd/< x y)) (partition 2 1 (take l tasks)))
+       (init-jobs (drop l tasks) (next lengths))))
+    succeed))
+
+(defn solvo*
+  "Find a feasible ordering for tasks.
+  tasks **MUST** be sorted by `:job`, then by `:id`."
+  [tasks]
+  (let [n (count tasks)
+        lengths (->> tasks (partition-by :job) (map count))
+        vars (repeatedly n lvar)]
+    (run* [q]
+          (== q vars)
+          (init-jobs vars lengths)
+          (everyg #(fd/in % (fd/interval (- n 1))) vars)
+          (fd/distinct vars))))
+
+(defn constructo*
+  "Wraps solvo*. Associates a sequence of ords with the matching tasks.
+  Returns a lazy sequence."
+  [tasks]
+  (let [ts        (sort-tasks tasks)
+        feasible* (solvo* ts)]
+    (map (fn [tasks ords]
+           (sort-by :ord (map (fn [t ord]
+                                (assoc t :ord ord))
+                              tasks
+                              ords)))
+         (repeat ts)
+         feasible*)))
+
+;; ## Attempt to filter on `:endby` constraints
+
+;; I'm a bit worried about the (lack of) speed of this gargantuan filter function!
+(defn due-filter*
+  "Filters feasible task orderings by whether jobs will be done on time.
+  Returns a lazy sequence."
+  [tasks]
+  (keep (fn [tasks]
+            (reduce (fn [solution task]
+                      (let [; Destructuring bind to get useful info from task.
+                            {:keys [machine duration job id endby]} task
+
+                            ; Get minimum start time of task.
+                            ; Task cannot begin until prior tasks in job have been completed
+                            ; AND prior jobs on machine have been completed.
+                            end-time (+ duration (max
+                                                  (or (get-in solution [:machines machine]) 0)
+                                                  (or (get-in solution [:jobs job]) 0)))]
+
+                        ; Check if `:endby` constraint has been violated.
+                        (if (and endby (< endby end-time))
+                          (reduced nil)
+                        ; Update solution with task on appropriate machine and new
+                        ; total processing time for job
+                          (-> solution
+                              (assoc-in [:machines machine] end-time)
+                              (assoc-in [:jobs job] end-time)
+                              (update :tasks #(conj % (assoc task :end-time end-time)))))))
+                    {}
+                    tasks))
+
+          (constructo* tasks)))
+
+(defn get-workspan
+  "Get the workspan of a solution."
+  [solution]
+  (->> solution
+       :machines
+       (map second)
+       (apply max)))
+
+(defn optimal-schedule
+  "Searches for a solution and returns the best found in `timeout` milliseconds.
+  \"Best\" means the minimum workspan, given the `:endby` constraints encoded into the tasks."
+  [millis tasks]
+  (loop [time          (System/currentTimeMillis)
+         [head & tail] (due-filter* tasks)
+         best          head]
+
+    (if (or (not head) (< millis (- (System/currentTimeMillis) time)))
+      best
+      (let [best (if (< (get-workspan head) (get-workspan best))
+                   head
+                   best)]
+        (recur time tail best)))))
---
+++ test/schejule/core_test.clj
@@ -0,0 +1,94 @@
+(ns schejule.core-test
+  (:require [clojure.test :refer :all]
+            [schejule.core :refer :all]))
+
+(def sample-tasks-1
+  [{:id 1 :machine 1 :job "yum" :duration 100}
+   {:id 2 :machine 2 :job "yum" :duration 80}
+   {:id 4 :machine 1 :job "yuck" :duration 130 :endby 250}])
+
+(def sample-tasks-2
+  [{:id 1 :job :client001 :duration 180 :machine 1}
+   {:id 2 :job :client001 :duration 120 :machine 2}
+   {:id 7 :job :stuff001 :duration 100 :machine 3}
+   {:id 8 :job :stuff001 :duration 150 :machine 4}
+   {:id 9 :job :job001 :duration 180 :machine 1}
+   {:id 13 :job :thing001 :duration 240 :machine 3}
+   {:id 17 :job :thing001 :duration 223 :machine 3}])
+
+(def sample-tasks-3
+  [{:id 1 :job :client001 :duration 180 :machine 1}
+   {:id 2 :job :client001 :duration 120 :machine 2}
+   {:id 3 :job :client001 :duration 100 :machine 3}
+   {:id 4 :job :client001 :duration 150 :machine 4 :endby 431}
+   {:id 5 :job :stuff001 :duration 180 :machine 1}
+   {:id 6 :job :stuff001 :duration 120 :machine 2}
+   {:id 7 :job :stuff001 :duration 100 :machine 3}
+   {:id 8 :job :stuff001 :duration 150 :machine 4}
+   {:id 9 :job :job001 :duration 180 :machine 1}
+   {:id 10 :job :job001 :duration 120 :machine 2}
+   {:id 11 :job :job001 :duration 100 :machine 3}
+   {:id 12 :job :job001 :duration 120 :machine 3 :endby 640}
+   {:id 13 :job :thing001 :duration 240 :machine 3}
+   {:id 14 :job :thing001 :duration 140 :machine 2}
+   {:id 15 :job :thing001 :duration 200 :machine 4}
+   {:id 16 :job :thing001 :duration 136 :machine 1}
+   {:id 17 :job :thing001 :duration 223 :machine 3 :endby 50000}])
+
+(deftest solvo*-test
+  (is (= (first (solvo* (sort-tasks sample-tasks-1)))
+         '(0 1 2)))
+  (is (= (first (solvo* (sort-tasks sample-tasks-2)))
+         '(0 1 2 3 4 5 6)))
+  (is (= (first (solvo* (sort-tasks sample-tasks-3)))
+       '(0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16))))
+
+(deftest constructo*-test
+  (is (= (first (constructo* sample-tasks-1))
+         '({:id 1, :machine 1, :job "yum", :duration 100, :ord 0}
+           {:id 2, :machine 2, :job "yum", :duration 80, :ord 1}
+           {:id 4, :machine 1, :job "yuck", :duration 130, :endby 250, :ord 2})))
+  (is (= (first (constructo* sample-tasks-2))
+         '({:id 1, :job :client001, :duration 180, :machine 1, :ord 0}
+           {:id 2, :job :client001, :duration 120, :machine 2, :ord 1}
+           {:id 7, :job :stuff001, :duration 100, :machine 3, :ord 2}
+           {:id 8, :job :stuff001, :duration 150, :machine 4, :ord 3}
+           {:id 9, :job :job001, :duration 180, :machine 1, :ord 4}
+           {:id 13, :job :thing001, :duration 240, :machine 3, :ord 5}
+           {:id 17, :job :thing001, :duration 223, :machine 3, :ord 6})))
+  (is (= (first (constructo* sample-tasks-3))
+         '({:id 1, :job :client001, :duration 180, :machine 1, :ord 0}
+           {:id 2, :job :client001, :duration 120, :machine 2, :ord 1}
+           {:id 3, :job :client001, :duration 100, :machine 3, :ord 2}
+           {:id 4, :job :client001, :duration 150, :machine 4, :endby 431, :ord 3}
+           {:id 5, :job :stuff001, :duration 180, :machine 1, :ord 4}
+           {:id 6, :job :stuff001, :duration 120, :machine 2, :ord 5}
+           {:id 7, :job :stuff001, :duration 100, :machine 3, :ord 6}
+           {:id 8, :job :stuff001, :duration 150, :machine 4, :ord 7}
+           {:id 9, :job :job001, :duration 180, :machine 1, :ord 8}
+           {:id 10, :job :job001, :duration 120, :machine 2, :ord 9}
+           {:id 11, :job :job001, :duration 100, :machine 3, :ord 10}
+           {:id 12, :job :job001, :duration 120, :machine 3, :endby 640, :ord 11}
+           {:id 13, :job :thing001, :duration 240, :machine 3, :ord 12}
+           {:id 14, :job :thing001, :duration 140, :machine 2, :ord 13}
+           {:id 15, :job :thing001, :duration 200, :machine 4, :ord 14}
+           {:id 16, :job :thing001, :duration 136, :machine 1, :ord 15}
+           {:id 17, :job :thing001, :duration 223, :machine 3, :endby 50000, :ord 16}))))
+
+(deftest due-filter*-test
+  (is (= (first (due-filter* sample-tasks-2))
+         {:machines {1 360, 2 300, 3 563, 4 250},
+          :jobs {:client001 300, :stuff001 250, :job001 360, :thing001 563},
+          :tasks '({:id 17, :job :thing001, :duration 223, :machine 3, :ord 6, :end-time 563}
+                  {:id 13, :job :thing001, :duration 240, :machine 3, :ord 5, :end-time 340}
+                  {:id 9, :job :job001, :duration 180, :machine 1, :ord 4, :end-time 360}
+                  {:id 8, :job :stuff001, :duration 150, :machine 4, :ord 3, :end-time 250}
+                  {:id 7, :job :stuff001, :duration 100, :machine 3, :ord 2, :end-time 100}
+                  {:id 2, :job :client001, :duration 120, :machine 2, :ord 1, :end-time 300}
+                  {:id 1, :job :client001, :duration 180, :machine 1, :ord 0, :end-time 180})}))
+  (is (= (count (due-filter* sample-tasks-2))
+         630)))
+
+(deftest optimal-schedule-test
+  (is (= (with-timeout 100 (optimal-schedule 100 sample-tasks-3))
+         nil)))
