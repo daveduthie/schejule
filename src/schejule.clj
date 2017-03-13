d2a708c15f355bd91ce570f9441040397c2df70f
David Duthie <daveduthie@dd-l.local>
2017-03-14 01:39:52 +0800

    It works... for medium-sized problems
    core.logic still chokes on 50 tasks with two :endby constraints

--- src/schejule.clj
+++ src/schejule.clj
@@ -22,29 +22,188 @@
            id            0]
       (if (empty? head)
         clean
-        (recur
-         (conj clean (assoc head :id id))
-         tail
-         (inc id))))))
+        (recur (conj clean (assoc head :id id)) tail (inc id))))))
 
 (defn by-job [tasks]
   (into {} (map (fn [task] [(:id task) (:job task)])
                 tasks)))
 
+;; ## Entrypoint
+
 (defn adequate-schedule
   "Searches for a solution and returns the best found in `timeout` milliseconds.
   \"Best\" means the minimum workspan, given the `:endby` constraints encoded into the tasks."
   [tasks millis]
-  (let [sols       (a/chan 1000)
-        tasks      (clean-tasks tasks)
-        job-lookup (by-job tasks)]
+  (let [tasks      (clean-tasks tasks)
+        job-lookup (by-job tasks)
+        failures   (agent nil)
+        sols       (agent nil)
+        f
+        (future (loop [rules        []
+                       [s & others] (z/simulate-schedule* tasks [])]
 
-    ; place solutions on channel
-    (>!! sols :yohoho)
+                  (if (not (Thread/interrupted))
+                    (if (s :failure)
+                      (let [new-rules (z/accrete-rules job-lookup rules (z/distil-rule s))]
+                        (prn (str "Failure: " (s :failure)))
+                        (prn (str "New Rules: " new-rules))
+                        (send failures (fn [old] (update old (s :failure) #(inc (or % 0)))))
+                        (recur new-rules (z/simulate-schedule* tasks new-rules)))
+                      (let [current (z/get-workspan s)]
+                        (prn (str "Success: " current))
+                        (send sols (fn [b] (if (or (nil? b)
+                                                  (< current (:workspan b)))
+                                            (assoc s :workspan current)
+                                            b)))
+                        (recur rules others)))
+                    nil)))]
 
-    ; take solutions from channel retaining best
-    (<!! sols)))
+    (Thread/sleep millis)
+    (future-cancel f)
+    (assoc @sols :failures @failures)))
 
-;; (use 'schejule.core-test)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(use 'schejule-test)
+;; (def failures (atom nil))
+;; (add-watch failures :yo (fn [_ _ _ state] (prn state)))
+;; (adequate-schedule schejule-test/sample-1 500)
+;; (adequate-schedule schejule-test/sample-3 (* 60000 5))
 
-;; (adequate-schedule sample-2 600)
+;; (def a-big-failure
+;;   {:machines {5 215, 11 311, 4 781, 7 946, 9 1099},
+;;    :jobs {"brq890" 946, "jtj163" 927, "pke479" 1099},
+;;    :tasks '({:id 7, :job "pke479", :machine 9, :duration 172, :endby 600, :ord 7, :end-time 1099}
+;;             {:id 6, :job "jtj163", :machine 9, :duration 146, :ord 6, :end-time 927}
+;;             {:id 5, :job "jtj163", :machine 4, :duration 96, :ord 5, :end-time 781}
+;;             {:id 4, :job "brq890", :machine 7, :duration 261, :ord 4, :end-time 946}
+;;             {:id 3, :job "brq890", :machine 4, :duration 84, :ord 3, :end-time 685}
+;;             {:id 2, :job "brq890", :machine 4, :duration 290, :ord 2, :end-time 601}
+;;             {:id 1, :job "brq890", :machine 11, :duration 96, :ord 1, :end-time 311}
+;;             {:id 0, :job "brq890", :machine 5, :duration 215, :ord 0, :end-time 215}),
+;;    :failure 7})
+
+;; (distil-rule a-big-failure)
+;; ((3 5) (6 7))
+
+;; (def a-big-failure-2
+;;   {:machines {5 215, 11 311, 4 781, 7 1042, 9 1015},
+;;    :jobs {"brq890" 1042, "jtj163" 843, "pke479" 1015},
+;;    :tasks '({:id 7, :job "pke479", :machine 9, :duration 172, :endby 600, :ord 7, :end-time 1015}
+;;             {:id 6, :job "jtj163", :machine 9, :duration 146, :ord 6, :end-time 843}
+;;             {:id 4, :job "brq890", :machine 7, :duration 261, :ord 5, :end-time 1042}
+;;             {:id 3, :job "brq890", :machine 4, :duration 84, :ord 4, :end-time 781}
+;;             {:id 5, :job "jtj163", :machine 4, :duration 96, :ord 3, :end-time 697}
+;;             {:id 2, :job "brq890", :machine 4, :duration 290, :ord 2, :end-time 601}
+;;             {:id 1, :job "brq890", :machine 11, :duration 96, :ord 1, :end-time 311}
+;;             {:id 0, :job "brq890", :machine 5, :duration 215, :ord 0, :end-time 215}),
+;;    :failure 7})
+
+;; (distil-rule a-big-failure-2)
+;; ((2 5) (6 7))
+
+;; ;=>(0 1 2 5 6 7)
+
+;; (accrete-rules (schejule/by-job (:tasks a-big-failure-2))
+;;                '(((3 5) (6 7)))
+;;                '((2 5) (6 7)))
+;; ;=>((0 1 2 5 6 7))
+
+;; (some #(and (% :failure) %) (simulate-schedule* sample-1-cleaned ['(0 1 2 5 6 7)]))
+
+;; (def a-big-failure-3
+;;   {:machines {5 1918, 11 311, 4 1983, 7 1212, 9 812, 0 114, 3 1429, 6 880},
+;;    :jobs {"brq890" 946, "jtj163" 242, "pke479" 414, "uym843" 880, "wim276" 1983},
+;;    :tasks '({:id 17, :job "wim276", :machine 4, :duration 65, :endby 1037, :ord 17, :end-time 1983}
+;;             {:id 16, :job "wim276", :machine 5, :duration 234, :ord 16, :end-time 1918}
+;;             {:id 15, :job "wim276", :machine 5, :duration 255, :ord 15, :end-time 1684}
+;;             {:id 14, :job "wim276", :machine 3, :duration 217, :ord 14, :end-time 1429}
+;;             {:id 13, :job "wim276", :machine 7, :duration 266, :ord 13, :end-time 1212}
+;;             {:id 12, :job "uym843", :machine 6, :duration 68, :endby 6800, :ord 12, :end-time 880}
+;;             {:id 11, :job "uym843", :machine 9, :duration 61, :ord 11, :end-time 812}
+;;             {:id 10, :job "uym843", :machine 3, :duration 293, :ord 10, :end-time 751}
+;;             {:id 9, :job "uym843", :machine 5, :duration 243, :ord 9, :end-time 458}
+;;             {:id 8, :job "uym843", :machine 0, :duration 114, :ord 8, :end-time 114}
+;;             {:id 7, :job "pke479", :machine 9, :duration 172, :endby 600, :ord 7, :end-time 414}
+;;             {:id 6, :job "jtj163", :machine 9, :duration 146, :ord 6, :end-time 242}
+;;             {:id 4, :job "brq890", :machine 7, :duration 261, :ord 5, :end-time 946}
+;;             {:id 3, :job "brq890", :machine 4, :duration 84, :ord 4, :end-time 685}
+;;             {:id 2, :job "brq890", :machine 4, :duration 290, :ord 3, :end-time 601}
+;;             {:id 5, :job "jtj163", :machine 4, :duration 96, :ord 2, :end-time 96}
+;;             {:id 1, :job "brq890", :machine 11, :duration 96, :ord 1, :end-time 311}
+;;             {:id 0, :job "brq890", :machine 5, :duration 215, :ord 0, :end-time 215}),
+;;    :failure 17})
+
+;; (z/distil-rule a-big-failure-3)
+;; ((4 13))
+
+;; ;=>(0 1 2 3 4 13 14 15 16 17)
+
+;; (z/accrete-rules (by-job (:tasks a-big-failure-3)) '(((2 5) (6 7))) '((4 13)))
+;; (((4 13)) ((2 5) (6 7)))
+
+;; ;=>((0 1 2 3 4 13 14 15 16 17) (0 1 2 5 6 7))
+
+;; (some #(and (% :failure) %) (simulate-schedule* sample-1-cleaned '((0 1 2 3 4 13 14 15 16 17) (0 1 2 5 6 7))))
+
+;; (def a-big-failure-4
+;;   {:machines {5 1457, 11 311, 4 1522, 7 946, 9 812, 0 114, 3 968, 6 880},
+;;    :jobs {"brq890" 946, "jtj163" 242, "wim276" 1522, "pke479" 414, "uym843" 880},
+;;    :tasks '({:id 17, :job "wim276", :machine 4, :duration 65, :endby 1037, :ord 17, :end-time 1522}
+;;             {:id 16, :job "wim276", :machine 5, :duration 234, :ord 16, :end-time 1457}
+;;             {:id 15, :job "wim276", :machine 5, :duration 255, :ord 15, :end-time 1223}
+;;             {:id 14, :job "wim276", :machine 3, :duration 217, :ord 14, :end-time 968}
+;;             {:id 12, :job "uym843", :machine 6, :duration 68, :endby 6800, :ord 13, :end-time 880}
+;;             {:id 11, :job "uym843", :machine 9, :duration 61, :ord 12, :end-time 812}
+;;             {:id 10, :job "uym843", :machine 3, :duration 293, :ord 11, :end-time 751}
+;;             {:id 9, :job "uym843", :machine 5, :duration 243, :ord 10, :end-time 458}
+;;             {:id 8, :job "uym843", :machine 0, :duration 114, :ord 9, :end-time 114}
+;;             {:id 7, :job "pke479", :machine 9, :duration 172, :endby 600, :ord 8, :end-time 414}
+;;             {:id 6, :job "jtj163", :machine 9, :duration 146, :ord 7, :end-time 242}
+;;             {:id 4, :job "brq890", :machine 7, :duration 261, :ord 6, :end-time 946}
+;;             {:id 13, :job "wim276", :machine 7, :duration 266, :ord 5, :end-time 266}
+;;             {:id 3, :job "brq890", :machine 4, :duration 84, :ord 4, :end-time 685}
+;;             {:id 2, :job "brq890", :machine 4, :duration 290, :ord 3, :end-time 601}
+;;             {:id 5, :job "jtj163", :machine 4, :duration 96, :ord 2, :end-time 96}
+;;             {:id 1, :job "brq890", :machine 11, :duration 96, :ord 1, :end-time 311}
+;;             {:id 0, :job "brq890", :machine 5, :duration 215, :ord 0, :end-time 215}),
+;;    :failure 17})
+
+;; (z/distil-rule a-big-failure-4)
+;; ((0 9) (10 14))
+
+;; (z/accrete-rules (by-job (:tasks a-big-failure-4))
+;;                '(((4 13))
+;;                  ((2 5) (6 7)))
+;;                '((0 9) (10 14)))
+
+;; (((0 9) (10 14)) ((4 13)) ((2 5) (6 7)))
+
+;; (def a-big-failure-5
+;;   {:machines {4 1421, 9 711, 0 114, 5 1356, 7 1303, 11 668, 3 867, 6 779},
+;;    :jobs {"jtj163" 242, "pke479" 414, "uym843" 779, "wim276" 1421, "brq890" 1303},
+;;    :tasks '({:id 17, :job "wim276", :machine 4, :duration 65, :endby 1037, :ord 17, :end-time 1421}
+;;             {:id 16, :job "wim276", :machine 5, :duration 234, :ord 16, :end-time 1356}
+;;             {:id 15, :job "wim276", :machine 5, :duration 255, :ord 15, :end-time 1122}
+;;             {:id 14, :job "wim276", :machine 3, :duration 217, :ord 14, :end-time 867}
+;;             {:id 12, :job "uym843", :machine 6, :duration 68, :endby 6800, :ord 13, :end-time 779}
+;;             {:id 11, :job "uym843", :machine 9, :duration 61, :ord 12, :end-time 711}
+;;             {:id 10, :job "uym843", :machine 3, :duration 293, :ord 11, :end-time 650}
+;;             {:id 4, :job "brq890", :machine 7, :duration 261, :ord 10, :end-time 1303}
+;;             {:id 3, :job "brq890", :machine 4, :duration 84, :ord 9, :end-time 1042}
+;;             {:id 2, :job "brq890", :machine 4, :duration 290, :ord 8, :end-time 958}
+;;             {:id 1, :job "brq890", :machine 11, :duration 96, :ord 7, :end-time 668}
+;;             {:id 0, :job "brq890", :machine 5, :duration 215, :ord 6, :end-time 572}
+;;             {:id 13, :job "wim276", :machine 7, :duration 266, :ord 5, :end-time 266}
+;;             {:id 9, :job "uym843", :machine 5, :duration 243, :ord 4, :end-time 357}
+;;             {:id 8, :job "uym843", :machine 0, :duration 114, :ord 3, :end-time 114}
+;;             {:id 7, :job "pke479", :machine 9, :duration 172, :endby 600, :ord 2, :end-time 414}
+;;             {:id 6, :job "jtj163", :machine 9, :duration 146, :ord 1, :end-time 242}
+;;             {:id 5, :job "jtj163", :machine 4, :duration 96, :ord 0, :end-time 96}),
+;;    :failure 17})
+
+;; (z/distil-rule a-big-failure-5)
+;; ((10 14))
+
+;; (z/accrete-rules (by-job (:tasks a-big-failure-5))
+;;                '(((0 9) (10 14)) ((4 13)) ((2 5) (6 7)))
+;;                '((10 14)))
--- test/schejule_test.clj
+++ test/schejule_test.clj
@@ -5,7 +5,7 @@
 ;; helpers to generate sample data
 (def alphabet (map char (range 97 123)))
 
-(defn- gen-name []
+(defn gen-name []
   (str (rand-nth alphabet)
        (rand-nth alphabet)
        (rand-nth alphabet)
@@ -13,7 +13,7 @@
        (rand-int 10)
        (rand-int 10)))
 
-(defn- gen-job
+(defn gen-job
   "Generate a sample job"
   [n start]
   (let [job-name (gen-name)]
@@ -25,7 +25,7 @@
             []
             (range n))))
 
-(defn- gen-joblist
+(defn gen-joblist
   [n]
   (reduce (fn [jobs x]
             (if (= n (count jobs))
@@ -46,11 +46,6 @@
    {:id 3, :job "brq890", :machine 4, :duration 84}
    {:id 4, :job "brq890", :machine 7, :duration 261}
 
-   ;; {:id 5, :job "dct869", :machine 6, :duration 74}
-   ;; {:id 6, :job "dct869", :machine 6, :duration 139}
-   ;; {:id 7, :job "dct869", :machine 6, :duration 146}
-   ;; {:id 8, :job "dct869", :machine 1, :duration 201}
-
    {:id 9, :job "wim276", :machine 7, :duration 266}
    {:id 10, :job "wim276", :machine 3, :duration 217}
    {:id 11, :job "wim276", :machine 5, :duration 255}
@@ -61,7 +56,7 @@
    {:id 20, :job "uym843", :machine 5, :duration 243}
    {:id 21, :job "uym843", :machine 3, :duration 293}
    {:id 22, :job "uym843", :machine 9, :duration 61}
-   {:id 23, :job "uym843", :machine 6, :duration 68 :endby 6800}
+   {:id 23, :job "uym843", :machine 6, :duration 68 :endby 1000}
 
    {:id 27, :job "pke479", :machine 9, :duration 172 :endby 600}
 
@@ -74,6 +69,69 @@
    {:id 3, :job "xyo468", :machine 2, :duration 157}
    {:id 17, :job "xyo468", :machine 1, :duration 117 :endby 274}
    {:id 4, :job "qrs991", :machine 7, :duration 268}])
+
+(def sample-3
+  [{:id 0, :job "obu084", :machine 2, :duration 116}
+   {:id 1, :job "obu084", :machine 2, :duration 172}
+   {:id 2, :job "obu084", :machine 10, :duration 140}
+   {:id 3, :job "obu084", :machine 1, :duration 128}
+   {:id 4, :job "obu084", :machine 11, :duration 270}
+
+   {:id 5, :job "xcx565", :machine 3, :duration 268}
+   {:id 6, :job "xcx565", :machine 3, :duration 200}
+   {:id 7, :job "xcx565", :machine 4, :duration 131}
+   {:id 8, :job "xcx565", :machine 5, :duration 228}
+   {:id 9, :job "xcx565", :machine 1, :duration 107}
+
+   {:id 10, :job "bqd174", :machine 6, :duration 245}
+   {:id 11, :job "bqd174", :machine 4, :duration 295}
+   {:id 12, :job "bqd174", :machine 8, :duration 228}
+   {:id 13, :job "bqd174", :machine 4, :duration 109}
+   {:id 14, :job "bqd174", :machine 8, :duration 96}
+
+   {:id 15, :job "fyf071", :machine 0, :duration 91}
+   {:id 16, :job "fyf071", :machine 8, :duration 207}
+   {:id 17, :job "fyf071", :machine 9, :duration 72}
+   {:id 18, :job "fyf071", :machine 3, :duration 213}
+   {:id 19, :job "fyf071", :machine 7, :duration 131}
+
+   {:id 20, :job "umj484", :machine 0, :duration 98}
+   {:id 21, :job "umj484", :machine 8, :duration 130}
+   {:id 22, :job "umj484", :machine 8, :duration 80}
+   {:id 23, :job "umj484", :machine 3, :duration 64}
+   {:id 24, :job "umj484", :machine 6, :duration 288}
+
+   {:id 25, :job "tzj588", :machine 9, :duration 285}
+   {:id 26, :job "tzj588", :machine 9, :duration 299}
+   {:id 27, :job "tzj588", :machine 2, :duration 140}
+
+   {:id 28, :job "opm162", :machine 8, :duration 295}
+   {:id 29, :job "opm162", :machine 11, :duration 176}
+
+   {:id 30, :job "ufy692", :machine 0, :duration 242}
+   {:id 31, :job "ufy692", :machine 8, :duration 147}
+   {:id 32, :job "ufy692", :machine 9, :duration 82}
+   {:id 33, :job "ufy692", :machine 7, :duration 166}
+   {:id 34, :job "ufy692", :machine 7, :duration 151}
+
+   {:id 35, :job "ysx968", :machine 2, :duration 140}
+   {:id 36, :job "ysx968", :machine 4, :duration 65}
+   {:id 37, :job "ysx968", :machine 9, :duration 219}
+   {:id 38, :job "ysx968", :machine 7, :duration 219}
+   {:id 39, :job "ysx968", :machine 10, :duration 179}
+
+   {:id 40, :job "yuo576", :machine 5, :duration 136}
+   {:id 41, :job "yuo576", :machine 4, :duration 80}
+   {:id 42, :job "yuo576", :machine 11, :duration 257}
+   {:id 43, :job "yuo576", :machine 7, :duration 233}
+   {:id 44, :job "yuo576", :machine 5, :duration 169 :endby 1075}
+
+   {:id 45, :job "ybc374", :machine 10, :duration 116}
+   {:id 46, :job "ybc374", :machine 2, :duration 70}
+   {:id 47, :job "ybc374", :machine 4, :duration 249}
+   {:id 48, :job "ybc374", :machine 6, :duration 208}
+
+   {:id 49, :job "lgt782", :machine 6, :duration 275 :endby 350}])
 
 (def sample-2-cleaned
   [{:id 0, :job "aag368", :machine 9, :duration 271}
